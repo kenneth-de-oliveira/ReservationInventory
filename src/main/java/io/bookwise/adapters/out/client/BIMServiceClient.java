6b239657a9c5c169d84a1d0d4c0f81bbef6b27c5
Kenneth de Oliveira Soares <koliveiras@minsait.com>
2025-01-17 13:10:24 -0300

    refactor: Atualização no BIMServiceClient

--- src/main/java/io/bookwise/adapters/out/client/BIMServiceClient.java
+++ src/main/java/io/bookwise/adapters/out/client/BIMServiceClient.java
@@ -7,29 +7,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-/**
- * BIMClient is a Feign client interface for interacting with the Inventory Management Web Service (BIM-WS).
- * It provides methods to perform operations related to books, such as finding a book by its ISBN.
- *
- * <p>This client is configured to communicate with the BIM-WS service located at the specified URL.
- * The configuration for this client is provided by the {@link io.bookwise.framework.config.adapter.SoapAdapterConfig} class.</p>
- *
- * <p>Example usage:</p>
- * <pre>{@code
- * @Autowired
- * private BIMServiceClient serviceClient;
- *
- * SearchBookRequest request = new SearchBookRequest();
- * // set request parameters
- * BookResponse response = serviceClient.findByIsbn(request);
- * }</pre>
- *
- * @see io.bookwise.framework.config.adapter.SoapAdapterConfig
- * @author koliveiras
- */
 @FeignClient(
         name = "BIM-WS",
-        url = "http://localhost:8080/BIM-WS/InventoryManagementWS",
+        url = "${api.inventory-management.url}",
         configuration = SoapAdapterConfig.class
 )
 public interface BIMServiceClient {
