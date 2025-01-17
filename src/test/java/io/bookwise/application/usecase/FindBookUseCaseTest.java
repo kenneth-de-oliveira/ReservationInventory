6d8690a151a42d0b62276063cb74b8c67a4c0f47
Kenneth de Oliveira Soares <koliveiras@minsait.com>
2025-01-16 21:53:55 -0300

    refactor: Atualização no FindBookUseCaseTest

--- src/test/java/io/bookwise/application/usecase/FindBookUseCaseTest.java
+++ src/test/java/io/bookwise/application/usecase/FindBookUseCaseTest.java
@@ -1,11 +1,13 @@
 package io.bookwise.application.usecase;
 
 import io.bookwise.application.core.domain.Book;
+import io.bookwise.application.core.ports.out.FeatureTogglePortOut;
 import io.bookwise.application.core.ports.out.FindBookPortOut;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
@@ -19,6 +21,9 @@ class FindBookUseCaseTest {
     @Mock
     private FindBookPortOut findBookPortOut;
 
+    @Mock
+    private FeatureTogglePortOut featureTogglePortOut;
+
     @InjectMocks
     private FindBookUseCase findBookUseCase;
 
@@ -27,6 +32,7 @@ class FindBookUseCaseTest {
         String isbn = "1234567890";
         Book book = new Book();
         when(findBookPortOut.findIsbn(isbn)).thenReturn(Optional.of(book));
+        when(featureTogglePortOut.isEnabled(Mockito.anyString())).thenReturn(false);
 
         findBookUseCase.findIsbn(isbn);
 
