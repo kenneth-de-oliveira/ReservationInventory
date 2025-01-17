26f8efe20599b91020910a54ce97cfe1b9498af3
Kenneth de Oliveira Soares <koliveiras@minsait.com>
2025-01-17 14:55:05 -0300

    refactor: Atualização no FindAddressByPostalCodeClient

--- src/main/java/io/bookwise/adapters/out/client/FindAddressByPostalCodeClient.java
+++ src/main/java/io/bookwise/adapters/out/client/FindAddressByPostalCodeClient.java
@@ -1,17 +1,12 @@
 package io.bookwise.adapters.out.client;
 
+import feign.Param;
+import feign.RequestLine;
 import io.bookwise.adapters.out.client.dto.AddressResponse;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(
-        name = "FindAddressByPostalCodeClient",
-        url = "${api.viacep.url}"
-)
 public interface FindAddressByPostalCodeClient {
 
-    @GetMapping("/{postalCode}/json")
-    AddressResponse find(@PathVariable String postalCode);
+    @RequestLine("GET /{postalCode}/json")
+    AddressResponse find(@Param("postalCode") String postalCode);
 
 }
--- src/main/java/io/bookwise/framework/config/adapter/CreateStudentAdapterConfig.java
+++ src/main/java/io/bookwise/framework/config/adapter/CreateStudentAdapterConfig.java
@@ -1,14 +1,23 @@
 package io.bookwise.framework.config.adapter;
 
+import feign.Feign;
+import feign.jackson.JacksonDecoder;
+import feign.jackson.JacksonEncoder;
 import io.bookwise.adapters.out.CreateStudentAdapterOut;
 import io.bookwise.adapters.out.FindAddressByPostalCodeAdapterOut;
+import io.bookwise.adapters.out.client.FindAddressByPostalCodeClient;
 import io.bookwise.application.core.ports.out.SendMailMessagePortOut;
 import io.bookwise.application.usecase.CreateStudentUseCase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CreateStudentAdapterConfig {
+
+    @Value("${api.viacep.url}")
+    private String url;
+
     @Bean
     public CreateStudentUseCase createStudentUseCase(
                 CreateStudentAdapterOut createStudentAdapterOut,
@@ -19,4 +28,13 @@ public class CreateStudentAdapterConfig {
                 findAddressByPostalCodeAdapterOut,
                 sendMailMessagePortOut);
     }
+
+    @Bean
+    public FindAddressByPostalCodeClient findAddressByPostalCodeClient() {
+        return Feign.builder()
+                .encoder(new JacksonEncoder())
+                .decoder(new JacksonDecoder())
+                .target(FindAddressByPostalCodeClient.class, url);
+    }
+
 }
