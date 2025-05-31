59c49ca221c44ace855c67c5c02541be793de91f
Kenneth de Oliveira Soares <koliveiras@minsait.com>
2025-05-31 11:56:34 -0300

    refactor: Alteração de Melhoria no FindAddressByPostalCodeClientImpl.java

--- src/main/java/io/bookwise/adapters/out/client/impl/FindAddressByPostalCodeClientImpl.java
+++ src/main/java/io/bookwise/adapters/out/client/impl/FindAddressByPostalCodeClientImpl.java
@@ -6,36 +6,52 @@ import io.bookwise.adapters.out.client.dto.AddressResponse;
 import io.bookwise.framework.errors.GenericErrorsEnum;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class FindAddressByPostalCodeClientImpl implements FindAddressByPostalCodeClient {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(FindAddressByPostalCodeClientImpl.class);
-
     private final FindAddressByPostalCodeClient findAddressByPostalCodeClient;
 
     @Override
     public AddressResponse find(String postalCode) {
-        try {
-            AddressResponse addressResponse = findAddressByPostalCodeClient.find(postalCode);
-            log.info("Address found by postal code: {}", addressResponse);
-            return addressResponse;
-        } catch (FeignException ex) {
-            if (GenericErrorsEnum.BAD_REQUEST.getCode().equals(String.valueOf(ex.status()))) {
-                LOGGER.error("Postal code not found: {}", ex.getMessage());
-                throw new RuntimeException("Postal code not found");
+        return handleExceptions(
+            () -> {
+                var addressResponse = findAddressByPostalCodeClient.find(postalCode);
+                log.info("Address found by postal code: {}", addressResponse);
+                return addressResponse;
+            },
+            ex -> {
+                if (ex instanceof FeignException && GenericErrorsEnum.BAD_REQUEST.getCode().equals(String.valueOf(((FeignException) ex).status()))) {
+                    handleMessageException("Postal code not found", ex);
+                    throw new RuntimeException("Postal code not found");
+                }
+                if (ex instanceof FeignException) {
+                    handleMessageException("Error finding address by postal code", ex);
+                    throw new RuntimeException("Error finding address by postal code: " + ex.getMessage());
+                }
+                handleMessageException(GenericErrorsEnum.ERROR_GENERIC.getInfo(), ex);
+                throw new RuntimeException(GenericErrorsEnum.ERROR_GENERIC.getInfo());
             }
-            LOGGER.error("Error finding address by postal code: {}", ex.getMessage());
-            throw new RuntimeException("Error finding address by postal code", ex);
+        );
+    }
+
+    private <T> T handleExceptions(Supplier<T> supplier, Consumer<Exception> exceptionHandler) {
+        try {
+            return supplier.get();
         } catch (Exception ex) {
-            LOGGER.error("Error finding address by postal code: {}", ex.getMessage());
-            throw new RuntimeException(GenericErrorsEnum.ERROR_GENERIC.getInfo());
+            exceptionHandler.accept(ex);
+            throw ex;
         }
+    }
+
+    private void handleMessageException(String msg, Exception ex) {
+        log.error("{}: {}", msg, ex.getMessage());
     }
 
 }
