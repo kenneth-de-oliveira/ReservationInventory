f3cbd26b3b4cd7974adab32f962fdbfa38fd262b
Kenneth de Oliveira Soares <koliveiras@minsait.com>
2025-05-31 10:38:10 -0300

    refactor: Atualização na classe InventoryManagementClientImpl e inclusão do GSON da Google.

--- src/main/java/io/bookwise/adapters/out/client/impl/InventoryManagementClientImpl.java
+++ src/main/java/io/bookwise/adapters/out/client/impl/InventoryManagementClientImpl.java
@@ -1,6 +1,7 @@
 package io.bookwise.adapters.out.client.impl;
 
 import com.example.inventorymanagement.*;
+import com.google.gson.Gson;
 import feign.FeignException;
 import io.bookwise.adapters.out.client.BIMServiceClient;
 import io.bookwise.adapters.out.client.InventoryManagementClient;
@@ -10,88 +11,111 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class InventoryManagementClientImpl implements InventoryManagementClient {
 
+    private final Gson gson;
     private final BIMServiceClient serviceClient;
 
     @Override
     public BookResponse findByIsbn(SearchBookRequest request) {
-
-        try {
-            var response = serviceClient.findByIsbn(request);
-            log.debug("Book found by isbn: {}", response);
-
-            return response;
-        } catch (FeignException ex) {
-            if (ex.getMessage().contains("Book not found")) {
-                this.handleMessageException("Book not found: ", ex);
-                throw new RuntimeException("Book not found");
-            }
-            this.handleMessageException("Error finding Book by isbn: ", ex);
-            throw new RuntimeException("Error finding Book by isbn: " + ex.getMessage());
-        } catch (Exception ex) {
-            this.handleMessageException(GenericErrorsEnum.ERROR_GENERIC.getInfo(), ex);
-            throw new RuntimeException(GenericErrorsEnum.ERROR_GENERIC.getInfo());
-        }
-
+        return handleExceptions(
+                () -> Optional.ofNullable(serviceClient.findByIsbn(request))
+                        .stream()
+                        .peek(response -> log.debug("Book found by isbn: {}", gson.toJson(response)))
+                        .findFirst()
+                        .orElse(null),
+                ex -> {
+                    if (ex instanceof FeignException && ex.getMessage().contains("Book not found")) {
+                        handleMessageException("Book not found: ", ex);
+                        throw new RuntimeException("Book not found");
+                    }
+                    if (ex instanceof FeignException) {
+                        handleMessageException("Error finding Book by isbn: ", ex);
+                        throw new RuntimeException("Error finding Book by isbn: " + ex.getMessage());
+                    }
+                    handleMessageException(GenericErrorsEnum.ERROR_GENERIC.getInfo(), ex);
+                    throw new RuntimeException(GenericErrorsEnum.ERROR_GENERIC.getInfo());
+                }
+        );
     }
 
     @Override
     public BookResponse findAll() {
-        try {
-            var request = new RetrieveAllBooksRequest();
-            var response = serviceClient.findAll(request);
-            log.info("Books found: {}", response);
-
-            return response;
-        } catch (FeignException ex) {
-            if (ex.getMessage().contains("No books found")) {
-                this.handleMessageException("No books found: ", ex);
-                throw new RuntimeException("No books found");
-            }
-            this.handleMessageException("Error finding all books: ", ex);
-            throw new RuntimeException("Error finding all books: " + ex.getMessage());
-        } catch (Exception ex) {
-            this.handleMessageException(GenericErrorsEnum.ERROR_GENERIC.getInfo(), ex);
-            throw new RuntimeException(GenericErrorsEnum.ERROR_GENERIC.getInfo());
-        }
+        return handleExceptions(
+                () -> Stream.of(serviceClient.findAll(new RetrieveAllBooksRequest()))
+                        .peek(response -> log.info("Books found: {}", gson.toJson(response)))
+                        .findFirst()
+                        .orElse(null),
+                ex -> {
+                    if (ex instanceof FeignException && ex.getMessage().contains("No books found")) {
+                        handleMessageException("No books found: ", ex);
+                        throw new RuntimeException("No books found");
+                    }
+                    if (ex instanceof FeignException) {
+                        handleMessageException("Error finding all books: ", ex);
+                        throw new RuntimeException("Error finding all books: " + ex.getMessage());
+                    }
+                    handleMessageException(GenericErrorsEnum.ERROR_GENERIC.getInfo(), ex);
+                    throw new RuntimeException(GenericErrorsEnum.ERROR_GENERIC.getInfo());
+                }
+        );
     }
 
     @Override
     public CategoryResponse saveCategory(CategoryRequest categoryRequest) {
-        try {
-            var response = serviceClient.createCategory(categoryRequest);
-            log.info("Category created: {}", response);
-
-            return response;
-        } catch (FeignException ex) {
-            this.handleMessageException("Error creating category: ", ex);
-            throw new RuntimeException("Error creating category: " + ex.getMessage());
-        } catch (Exception ex) {
-            this.handleMessageException(GenericErrorsEnum.ERROR_GENERIC.getInfo(), ex);
-            throw new RuntimeException(GenericErrorsEnum.ERROR_GENERIC.getInfo());
-        }
+        return handleExceptions(
+                () -> Stream.of(serviceClient.createCategory(categoryRequest))
+                        .peek(response -> log.info("Category created: {}", gson.toJson(response)))
+                        .findFirst()
+                        .orElse(null),
+                ex -> {
+                    if (ex instanceof FeignException) {
+                        handleMessageException("Error creating category: ", ex);
+                        throw new RuntimeException("Error creating category: " + ex.getMessage());
+                    }
+                    handleMessageException(GenericErrorsEnum.ERROR_GENERIC.getInfo(), ex);
+                    throw new RuntimeException(GenericErrorsEnum.ERROR_GENERIC.getInfo());
+                }
+        );
     }
 
     @Override
     public void saveBook(BookRequest bookRequest) {
-        try {
-            serviceClient.createBook(bookRequest);
-            log.info("Book created: {}", bookRequest);
+        handleExceptions(
+                () -> {
+                    serviceClient.createBook(bookRequest);
+                    log.info("Book created: {}", gson.toJson(bookRequest));
+                    return null;
+                },
+                ex -> {
+                    if (ex instanceof FeignException && ex.getMessage().contains("Category not found")) {
+                        handleMessageException("Category not found: ", ex);
+                        throw new RuntimeException("Category not found");
+                    }
+                    if (ex instanceof FeignException) {
+                        handleMessageException("Error creating book: ", ex);
+                        throw new RuntimeException("Error creating book: " + ex.getMessage());
+                    }
+                    handleMessageException(GenericErrorsEnum.ERROR_GENERIC.getInfo(), ex);
+                    throw new RuntimeException(GenericErrorsEnum.ERROR_GENERIC.getInfo());
+                }
+        );
+    }
 
-        } catch (FeignException ex) {
-            if (ex.getMessage().contains("Category not found")) {
-                this.handleMessageException("Category not found: ", ex);
-                throw new RuntimeException("Category not found");
-            }
-            this.handleMessageException("Error creating book: ", ex);
-            throw new RuntimeException("Error creating book: " + ex.getMessage());
+    private <T> T handleExceptions(Supplier<T> supplier, Consumer<Exception> exceptionHandler) {
+        try {
+            return supplier.get();
         } catch (Exception ex) {
-            this.handleMessageException(GenericErrorsEnum.ERROR_GENERIC.getInfo(), ex);
-            throw new RuntimeException(GenericErrorsEnum.ERROR_GENERIC.getInfo());
+            exceptionHandler.accept(ex);
+            throw ex;
         }
     }
 
