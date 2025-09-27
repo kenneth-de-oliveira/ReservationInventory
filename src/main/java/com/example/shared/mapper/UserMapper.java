e5303a8e7fb1bbb4aa34f6ead65b693359fe7fd4
Kenneth de Oliveira Soares <koliveiras@minsait.com>
2025-09-27 14:24:30 -0300

    refactor: mapper's de User de adapter in

---
+++ src/main/java/com/example/shared/mapper/UserMapper.java
@@ -0,0 +1,14 @@
+package com.example.shared.mapper;
+
+import com.example.adapter.out.client.dto.UserResponse;
+import com.example.application.core.domain.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = AddressMapper.class, implementationName = "UserMapperAdapterInImpl")
+public interface UserMapper {
+
+    @Mapping(source = "address", target = "userAddressResponse")
+    UserResponse toResponse(User user);
+
+}
